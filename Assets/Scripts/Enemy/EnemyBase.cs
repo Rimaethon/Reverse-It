d0807a3429fc7ee21c0b5a45701e161ca457ce1b
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-05 17:52:05 +0300

    Comment deletion and reformatting

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -4,37 +4,36 @@ namespace Enemy
 {
     public abstract class EnemyBase : MonoBehaviour
     {
-        [Header("Settings")]
-        [Tooltip("How fast this enemy moves")]
+        public enum EnemyState
+        {
+            Walking,
+            Dead,
+            Idle
+        }
+
         public float moveSpeed = 2f;
 
-   
-        public enum EnemyState { Walking, Dead, Idle }
-
-        [Tooltip("The state the enemy is in for animation playback")]
         public EnemyState enemyState;
 
-  
+
         protected virtual void Start()
         {
             Setup();
         }
 
-   
+
         protected virtual void Update()
         {
-            // Every frame, get the desired movement of this enemy, then move it.
-            Vector3 movement = GetMovement();
+            var movement = GetMovement();
             MoveEnemy(movement);
         }
 
-   
+
         protected virtual void Setup()
         {
-
         }
 
-      
+
         protected virtual Vector3 GetMovement()
         {
             return Vector3.zero;
