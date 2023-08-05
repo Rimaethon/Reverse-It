f63a424e078bc7f0dcfb005ee741aecc04ce3d05
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-05 17:56:10 +0300

    Comment deletion reformatting unnecessary publicity removal

--- Assets/Scripts/Enemy/WalkingEnemy.cs
+++ Assets/Scripts/Enemy/WalkingEnemy.cs
@@ -4,48 +4,47 @@ using UnityEngine;
 
 namespace Enemy
 {
-    
     public sealed class WalkingEnemy : EnemyBase
     {
-   
-        public enum WalkDirections { Right, Left, None }
-    
+        public enum WalkDirections
+        {
+            Right,
+            Left,
+            None
+        }
 
+      
+        [SerializeField] private GroundCheck wallTestLeft;
 
-        [Header("References")]
-        [Tooltip("The ground check component used to test whether this enemy has hit a wall to the left.")]
-        public GroundCheck wallTestLeft;
-        [Tooltip("The ground check component used to test whether this enemy has hit a wall to the right.")]
-        public GroundCheck wallTestRight;
-        [Tooltip("Left ground check component used to determine when the enemy has reached an edge on its left.")]
-        public GroundCheck leftEdge;
-        [Tooltip("Right ground check component used to determine when the enemy has reached an edge on its right.")]
-        public GroundCheck rightEdge;
+        [SerializeField] private GroundCheck wallTestRight;
 
-        [Header("Settings")]
-        [Tooltip("The direction that this enemy walks in until it hits a wall")]
-        public WalkDirections walkDirection = WalkDirections.None;
-        [Tooltip("Whether this enemy will turn around if it detects an edge.")]
-        public bool willTurnAroundAtEdge;
+        [SerializeField] private GroundCheck leftEdge;
+
+        [SerializeField] private GroundCheck rightEdge;
+
+        [SerializeField] private WalkDirections walkDirection = WalkDirections.None;
+
+        [SerializeField] private bool willTurnAroundAtEdge;
+
         private Rigidbody2D _enemyRb;
 
         private SpriteRenderer _spriteRenderer;
 
-     
-        protected override void Setup()
-        {
-            base.Setup();
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-        }
 
-    
         protected override void Update()
         {
             DetermineWalkDirection();
             base.Update();
         }
 
-        
+
+        protected override void Setup()
+        {
+            base.Setup();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+
         protected override Vector3 GetMovement()
         {
             switch (walkDirection)
@@ -63,71 +62,51 @@ namespace Enemy
                     return base.GetMovement();
             }
         }
-    
 
-     
+
         private void DetermineWalkDirection()
         {
-            if (TestWall() || GetIsNearEdge())
-            {
-                TurnAround();
-            }
-            if (_spriteRenderer != null)
-            {
-                _spriteRenderer.flipX = (walkDirection == WalkDirections.Right);
-            }
+            if (TestWall() || GetIsNearEdge()) TurnAround();
+            if (_spriteRenderer != null) _spriteRenderer.flipX = walkDirection == WalkDirections.Right;
         }
 
         private void TurnAround()
         {
             if (walkDirection == WalkDirections.Left)
-            {
                 walkDirection = WalkDirections.Right;
-            }
-            else if (walkDirection == WalkDirections.Right)
-            {
-                walkDirection = WalkDirections.Left;
-            }
+            else if (walkDirection == WalkDirections.Right) walkDirection = WalkDirections.Left;
         }
 
-       
+
         private bool TestWall()
         {
             switch (walkDirection)
             {
                 case WalkDirections.Left:
-                    if (wallTestLeft != null)
-                    {
-                        return wallTestLeft.CheckGrounded();
-                    }
+                    if (wallTestLeft != null) return wallTestLeft.CheckGrounded();
                     break;
                 case WalkDirections.Right:
-                    if (wallTestRight != null)
-                    {
-                        return wallTestRight.CheckGrounded();
-                    }
+                    if (wallTestRight != null) return wallTestRight.CheckGrounded();
                     break;
                 case WalkDirections.None:
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
+
             return false;
         }
 
-      
+
         private bool GetIsNearEdge()
         {
-            GroundCheck check = walkDirection switch
+            var check = walkDirection switch
             {
                 WalkDirections.Left => leftEdge,
                 WalkDirections.Right => rightEdge,
                 _ => null
             };
-            if (check != null && !check.CheckGrounded())
-            {
-                return willTurnAroundAtEdge;
-            }
+            if (check != null && !check.CheckGrounded()) return willTurnAroundAtEdge;
             return false;
         }
     }
