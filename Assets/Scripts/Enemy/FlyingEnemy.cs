3731effe75626e13300ba082e7f68663d2a51e73
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-05 17:54:38 +0300

    fckn codebase was used as notepad I believe

--- Assets/Scripts/Enemy/FlyingEnemy.cs
+++ Assets/Scripts/Enemy/FlyingEnemy.cs
@@ -6,14 +6,18 @@ namespace Enemy
     [RequireComponent(typeof(WaypointMover))]
     public sealed class FlyingEnemy : EnemyBase
     {
-        [Header("References")]
-        [Tooltip("The waypoint mover component which does the work of moving this enemy")]
-        public WaypointMover waypointMover;
+        [SerializeField] private WaypointMover waypointMover;
 
-        // The sprite renderer associated with this enemy
         private SpriteRenderer _spriteRenderer;
 
-  
+
+        protected override void Update()
+        {
+            CheckFlipSprite();
+            SetStateInformation();
+        }
+
+
         protected override void Setup()
         {
             base.Setup();
@@ -21,33 +25,20 @@ namespace Enemy
             _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
-    
-        protected override void Update()
-        {
-            CheckFlipSprite();
-            SetStateInformation();
-        }
 
-   
         private void CheckFlipSprite()
         {
             if (waypointMover != null && _spriteRenderer != null)
-            {
-                _spriteRenderer.flipX = (Vector3.Dot(waypointMover.travelDirection, Vector3.right) < 0);
-            }
+                _spriteRenderer.flipX = Vector3.Dot(waypointMover.travelDirection, Vector3.right) < 0;
         }
 
 
         private void SetStateInformation()
         {
             if (waypointMover != null)
-            {
                 enemyState = waypointMover.stopped ? EnemyState.Idle : EnemyState.Walking;
-            }
             else
-            {
                 enemyState = EnemyState.Idle;
-            }
         }
     }
 }
