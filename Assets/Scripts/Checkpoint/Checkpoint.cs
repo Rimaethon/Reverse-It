9e5492749f7533791d19a4ca926cca488f2af7ca
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-05 17:51:28 +0300

    Just reformatting

--- Assets/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -10,23 +10,19 @@ namespace Checkpoint
         [SerializeField] private string animatorActiveParameter = "isActive";
         [SerializeField] private GameObject checkpointActivationEffect;
 
-   
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (!collision.CompareTag("Player") || collision.gameObject.GetComponent<Health>() == null) return;
-            
-            Health playerHealth = collision.gameObject.GetComponent<Health>();
+
+            var playerHealth = collision.gameObject.GetComponent<Health>();
             playerHealth.SetRespawnPoint(respawnLocation.position);
 
             if (CheckpointTracker.CurrentCheckpoint != null)
-            {
                 CheckpointTracker.CurrentCheckpoint.checkpointAnimator.SetBool(animatorActiveParameter, false);
-            }
 
             if (CheckpointTracker.CurrentCheckpoint != this && checkpointActivationEffect != null)
-            {
                 Instantiate(checkpointActivationEffect, transform.position, Quaternion.identity, null);
-            }
 
             CheckpointTracker.CurrentCheckpoint = this;
             checkpointAnimator.SetBool(animatorActiveParameter, true);
