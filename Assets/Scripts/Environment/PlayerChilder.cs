efa8fdcb92d3b6dba2b6f0261a7820fa9de286e0
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-05 18:00:59 +0300

    If statements simplified

--- Assets/Scripts/Environment/PlayerChilder.cs
+++ Assets/Scripts/Environment/PlayerChilder.cs
@@ -4,41 +4,33 @@ namespace Environment
 {
     public class PlayerChilder : MonoBehaviour
     {
-    
         private void OnTriggerEnter2D(Collider2D collision)
         {
             MakeAChildOfAttachedTransform(collision);
         }
 
-       
-        private void OnTriggerStay2D(Collider2D collision)
-        {
-            MakeAChildOfAttachedTransform(collision);
-        }
 
-       
         private void OnTriggerExit2D(Collider2D collision)
         {
             DeChild(collision);
         }
 
-       
-        private static void DeChild(Collider2D collision)
+
+        private void OnTriggerStay2D(Collider2D collision)
         {
-            if (collision.CompareTag("Player"))
-            {
-                collision.gameObject.transform.SetParent(null);
-            }
+            MakeAChildOfAttachedTransform(collision);
         }
 
 
-      
+        private static void DeChild(Collider2D collision)
+        {
+            if (collision.CompareTag("Player")) collision.gameObject.transform.SetParent(null);
+        }
+
+
         private void MakeAChildOfAttachedTransform(Collider2D collision)
         {
-            if (collision.CompareTag("Player"))
-            {
-                collision.gameObject.transform.SetParent(transform);
-            } 
+            if (collision.CompareTag("Player")) collision.gameObject.transform.SetParent(transform);
         }
     }
 }
