10ef456a8a57f850a586c1cc5ef00a7f7792b6a0
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-06 23:59:42 +0300

    Comments deleted

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,20 +1,21 @@
-﻿using UnityEngine;
+﻿using Core;
+using UnityEngine;
 
 namespace Player
 {
     public class PlayerAnimator : MonoBehaviour
     {
-        [Header("Settings")]
-        [Tooltip("The player controller script to read state information from")]
-        public PlayerController playerController;
-        [Tooltip("The animator component that controls the player's animations")]
-        public Animator animator;
-
         private static readonly int IsIdle = Animator.StringToHash("isIdle");
         private static readonly int IsJumping = Animator.StringToHash("isJumping");
         private static readonly int IsFalling = Animator.StringToHash("isFalling");
         private static readonly int IsWalking = Animator.StringToHash("isWalking");
         private static readonly int IsDead = Animator.StringToHash("isDead");
+
+        [Header("Settings")] [Tooltip("The player controller script to read state information from")]
+        public PlayerController playerController;
+
+        [Tooltip("The animator component that controls the player's animations")]
+        public Animator animator;
 
 
         private void Start()
@@ -31,20 +32,17 @@ namespace Player
 
         private void ReadPlayerStateAndAnimate()
         {
-            if (animator == null)
-            {
-                return;
-            }
+            if (animator == null) return;
 
-            animator.SetBool(IsIdle, playerController.state == PlayerController.PlayerState.Idle);
+            animator.SetBool(IsIdle, playerController.state == PlayerStates.Idle);
 
-            animator.SetBool(IsJumping, playerController.state == PlayerController.PlayerState.Jump);
+            animator.SetBool(IsJumping, playerController.state == PlayerStates.Jump);
 
-            animator.SetBool(IsFalling, playerController.state == PlayerController.PlayerState.Fall);
+            animator.SetBool(IsFalling, playerController.state == PlayerStates.Fall);
 
-            animator.SetBool(IsWalking, playerController.state == PlayerController.PlayerState.Walk);
+            animator.SetBool(IsWalking, playerController.state == PlayerStates.Walk);
 
-            animator.SetBool(IsDead, playerController.state == PlayerController.PlayerState.Dead);
+            animator.SetBool(IsDead, playerController.state == PlayerStates.Dead);
         }
     }
 }
