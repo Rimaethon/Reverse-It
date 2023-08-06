58504ca47fb668920496b0c2719808859738bdb5
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-06 23:59:17 +0300

    Formerlyserialized comments deleted

--- Assets/Scripts/Utility/ScreenshotUtility.cs
+++ Assets/Scripts/Utility/ScreenshotUtility.cs
@@ -5,73 +5,58 @@ using UnityEngine.Serialization;
 
 namespace Utility
 {
-   
     public class ScreenshotUtility : MonoBehaviour
     {
-        private static ScreenshotUtility _screenShotUtility;
+        #region Constants
 
-        #region Public Variables
-        [Header("Settings")]
-        [Tooltip("Should the screenshot utility run only in the editor.")]
-        public bool runOnlyInEditor = true;
-        [FormerlySerializedAs("m_ScreenshotKey")] [Tooltip("What key is mapped to take the screenshot.")]
-        public string mScreenshotKey = "c";
-        [FormerlySerializedAs("m_ScaleFactor")] [Tooltip("What is the scale factor for the screenshot. Standard is 1, 2x size is 2, etc..")]
-        public int mScaleFactor = 1;
-        [Tooltip("Include image size in filename.")]
-        public bool includeImageSizeInFilename = true;
+        private const string ImageCntKey = "IMAGE_CNT";
+
         #endregion
+
+        private static ScreenshotUtility _screenShotUtility;
 
         [FormerlySerializedAs("m_ImageCount")]
         [Header("Private Variables")]
+
         #region Private Variables
+
         // The number of screenshots taken
         [Tooltip("Use the Reset Counter contextual menu item to reset this.")]
         [SerializeField]
         private int mImageCount;
-        #endregion
 
-        #region Constants
-        private const string ImageCntKey = "IMAGE_CNT";
         #endregion
 
 
         private void Awake()
         {
             if (_screenShotUtility != null)
-            { 
+            {
                 Destroy(gameObject);
             }
             else if (runOnlyInEditor && !Application.isEditor)
-            { 
+            {
                 Destroy(gameObject);
             }
             else
             {
-                _screenShotUtility = this.GetComponent<ScreenshotUtility>();
+                _screenShotUtility = GetComponent<ScreenshotUtility>();
 
                 DontDestroyOnLoad(gameObject);
 
                 mImageCount = PlayerPrefs.GetInt(ImageCntKey);
 
-                if (!Directory.Exists("Screenshots"))
-                {
-                    Directory.CreateDirectory("Screenshots");
-                }
+                if (!Directory.Exists("Screenshots")) Directory.CreateDirectory("Screenshots");
             }
         }
 
 
         private void Update()
         {
-
-            if (Keyboard.current.FindKeyOnCurrentKeyboardLayout(mScreenshotKey).wasPressedThisFrame)
-            {
-                TakeScreenshot();
-            }
+            if (Keyboard.current.FindKeyOnCurrentKeyboardLayout(mScreenshotKey).wasPressedThisFrame) TakeScreenshot();
         }
 
-     
+
         [ContextMenu("Reset Counter")]
         public void ResetCounter()
         {
@@ -84,18 +69,32 @@ namespace Utility
         {
             PlayerPrefs.SetInt(ImageCntKey, ++mImageCount);
 
-            int width = Screen.width * mScaleFactor;
-            int height = Screen.height * mScaleFactor;
+            var width = Screen.width * mScaleFactor;
+            var height = Screen.height * mScaleFactor;
 
-            string pathname = "Screenshots/Screenshot_";
-            if (includeImageSizeInFilename)
-            {
-                pathname += width + "x" + height + "_";
-            }
+            var pathname = "Screenshots/Screenshot_";
+            if (includeImageSizeInFilename) pathname += width + "x" + height + "_";
             pathname += mImageCount + ".png";
 
             ScreenCapture.CaptureScreenshot(pathname, mScaleFactor);
             Debug.Log("Screenshot captured at " + pathname);
         }
+
+        #region Public Variables
+
+        [Header("Settings")] [Tooltip("Should the screenshot utility run only in the editor.")]
+        public bool runOnlyInEditor = true;
+
+        [FormerlySerializedAs("m_ScreenshotKey")] [Tooltip("What key is mapped to take the screenshot.")]
+        public string mScreenshotKey = "c";
+
+        [FormerlySerializedAs("m_ScaleFactor")]
+        [Tooltip("What is the scale factor for the screenshot. Standard is 1, 2x size is 2, etc..")]
+        public int mScaleFactor = 1;
+
+        [Tooltip("Include image size in filename.")]
+        public bool includeImageSizeInFilename = true;
+
+        #endregion
     }
 }
