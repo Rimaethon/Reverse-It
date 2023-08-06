7c9516c9961e6fba9034942dd2473d7bd06e1861
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-06 23:54:18 +0300

    Comments Deleted and unnecessary public fields made private

--- Assets/Scripts/Pickups/ScorePickup.cs
+++ Assets/Scripts/Pickups/ScorePickup.cs
@@ -6,17 +6,13 @@ namespace Pickups
 {
     public class ScorePickup : Pickup
     {
-        [Header("Score Settings")]
-        [Tooltip("Amount of score to add when picked up")]
-        public int scoreAmount = 1;
+        [SerializeField] int scoreAmount = 1;
 
-    
+
         protected override void DoOnPickup(Collider2D collision)
         {
             if (collision.CompareTag("Player") && collision.gameObject.GetComponent<Health>() != null)
-            {
                 GameManager.AddScore(scoreAmount);
-            }
             base.DoOnPickup(collision);
         }
     }
