2def4676e67f7dcd588d199dc3bed965a9263b1d
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-06 23:53:56 +0300

    Comments Deleted and unnecessary public fields made private

--- Assets/Scripts/Pickups/ExtraLifePickup.cs
+++ Assets/Scripts/Pickups/ExtraLifePickup.cs
@@ -5,18 +5,17 @@ namespace Pickups
 {
     public class ExtraLifePickup : Pickup
     {
-        [Header("Extra Life Settings")]
-        [Tooltip("How many Lives to give")]
-        public int extraLives = 1;
+        [SerializeField] private int extraLives = 1;
 
 
         protected override void DoOnPickup(Collider2D collision)
         {
             if (collision.CompareTag("Player") && collision.gameObject.GetComponent<Health>() != null)
             {
-                Health playerHealth = collision.gameObject.GetComponent<Health>();
+                var playerHealth = collision.gameObject.GetComponent<Health>();
                 playerHealth.AddLives(extraLives);
             }
+
             base.DoOnPickup(collision);
         }
     }
