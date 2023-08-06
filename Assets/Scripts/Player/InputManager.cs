8ece1700418b83ffe893d917da9b9cd94968e602
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-06 23:59:31 +0300

    Input manager updated

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -6,27 +6,45 @@ namespace Player
 {
     public class InputManager : MonoBehaviour
     {
-        public static InputManager Instance; 
-        [SerializeField] GameObject player;
+        public static InputManager Instance;
+        [SerializeField] private GameObject player;
+
+
+        public float horizontalMovement;
+        public float verticalMovement;
+
+        [Header("Jump Input")] [Tooltip("Whether a jump was started this frame.")]
+        public bool jumpStarted;
+
+        [Tooltip("Whether the jump button is being held.")]
+        public bool jumpHeld;
+
+
+        public float pauseButton;
+
+
+        public float horizontalLookAxis;
+        public float verticalLookAxis;
         private PlayerController _playerscript;
 
-  
+        public InputManager()
+        {
+            jumpStarted = false;
+        }
+
+
         private void Awake()
         {
             ResetValuesToDefault();
             if (Instance == null)
-            {
                 Instance = this;
-            }
             else
-            {
-                Destroy(this.gameObject);
-            }
+                Destroy(gameObject);
         }
 
         private void Start()
         {
-            _playerscript=player.GetComponent<PlayerController>();
+            _playerscript = player.GetComponent<PlayerController>();
         }
 
         private void ResetValuesToDefault()
@@ -43,73 +61,47 @@ namespace Player
             pauseButton = default;
         }
 
-       
-        public float horizontalMovement;
-        public float verticalMovement;
 
- 
         public void GetMovementInput(InputAction.CallbackContext callbackContext)
         {
-            Vector2 movementVector = callbackContext.ReadValue<Vector2>();
+            var movementVector = callbackContext.ReadValue<Vector2>();
 
             horizontalMovement = movementVector.x;
             verticalMovement = movementVector.y;
         }
-    
-        [Header("Jump Input")]
-        [Tooltip("Whether a jump was started this frame.")]
-        public bool jumpStarted;
-        [Tooltip("Whether the jump button is being held.")]
-        public bool jumpHeld;
 
-   
+
         public void GetJumpInput(InputAction.CallbackContext callbackContext)
         {
             jumpStarted = !callbackContext.canceled;
             jumpHeld = !callbackContext.canceled;
-            if (Instance.isActiveAndEnabled)
-            {
-                StartCoroutine(nameof(ResetJumpStart));
-            } 
+            if (Instance.isActiveAndEnabled) StartCoroutine(nameof(ResetJumpStart));
         }
 
- 
+
         private IEnumerator ResetJumpStart()
         {
             yield return new WaitForEndOfFrame();
             jumpStarted = false;
         }
-    
-   
-        public float pauseButton;
 
-    
+
         public void GetPauseInput(InputAction.CallbackContext callbackContext)
         {
             pauseButton = callbackContext.ReadValue<float>();
-        }
-    
-    
-        public float horizontalLookAxis;
-        public float verticalLookAxis;
-
-        public InputManager()
-        {
-            jumpStarted = false;
         }
 
 
         public void GetMouseLookInput(InputAction.CallbackContext callbackContext)
         {
-            Vector2 mouseLookVector = callbackContext.ReadValue<Vector2>();
+            var mouseLookVector = callbackContext.ReadValue<Vector2>();
             horizontalLookAxis = mouseLookVector.x;
             verticalLookAxis = mouseLookVector.y;
         }
+
         public void GetGravityInput(InputAction.CallbackContext callbackContext)
         {
-           
             _playerscript.GravitatePlayer();
-        
         }
     }
 }
