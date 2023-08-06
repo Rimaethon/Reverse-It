06adf7112dfe7f464a723428b04bc7976fa8bb77
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-06 23:52:14 +0300

    Unnecessary Comments deleted

--- Assets/Scripts/Keys&Doors/Door.cs
+++ Assets/Scripts/Keys&Doors/Door.cs
@@ -3,55 +3,41 @@ using UnityEngine.Events;
 
 namespace Keys_Doors
 {
-    
     [RequireComponent(typeof(Collider2D))]
     public class Door : MonoBehaviour
     {
-        [Header("Settings")]
-        [Tooltip("The ID of the door used to determine which keys unlock it.\n" +
-                 "A door ID of 0 is unlocked by default.")]
+     
         public int doorID;
-        [Tooltip("Whether this door is open right now.")]
+
         public bool isOpen;
-        [Tooltip("Events to call when opening the door.")]
-        public UnityEvent openEvent = new UnityEvent();
-        [Tooltip("Events to call when closing the door.")]
-        public UnityEvent closeEvent = new UnityEvent();
-        [Tooltip("The effect to play when the door opens or closes")]
+
+        public UnityEvent openEvent = new();
+
+        public UnityEvent closeEvent = new();
+
         public GameObject doorOpenAndCloseEffect;
-        [Tooltip("The effect to play when the door is attempted to open but can not")]
+
         public GameObject doorLockedEffect;
 
-      
+
         protected virtual void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.transform.CompareTag("Player"))
-            {
-                AttemptToOpen();
-            }
+            if (collision.transform.CompareTag("Player")) AttemptToOpen();
         }
 
 
-       
         protected virtual void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.CompareTag("Player"))
-            {
-                AttemptToOpen();
-            }
+            if (collision.CompareTag("Player")) AttemptToOpen();
         }
 
 
         private void AttemptToOpen()
         {
             if (CheckPlayerHasKey() && !isOpen)
-            {
                 Open();
-            }
             else if (doorLockedEffect && !isOpen)
-            {
                 Instantiate(doorLockedEffect, transform.position, Quaternion.identity, null);
-            }
         }
 
 
@@ -60,15 +46,13 @@ namespace Keys_Doors
             return KeyRing.HasKey(this);
         }
 
- 
+
         protected virtual void Open()
         {
             isOpen = true;
             openEvent.Invoke();
             if (doorOpenAndCloseEffect)
-            {
                 Instantiate(doorOpenAndCloseEffect, transform.position, Quaternion.identity, null);
-            }
         }
 
         protected virtual void Close()
@@ -76,9 +60,7 @@ namespace Keys_Doors
             isOpen = false;
             closeEvent.Invoke();
             if (doorOpenAndCloseEffect)
-            {
                 Instantiate(doorOpenAndCloseEffect, transform.position, Quaternion.identity, null);
-            }
         }
     }
 }
