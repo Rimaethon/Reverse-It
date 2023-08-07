210fe16c1f506665beb8aca636069c96d726cce9
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-07 23:57:07 +0300

    Update LevelSwitcher.cs

--- Assets/Scripts/Utility/LevelSwitcher.cs
+++ Assets/Scripts/Utility/LevelSwitcher.cs
@@ -2,10 +2,8 @@
 
 namespace Utility
 {
-
     public class LevelSwitcher : MonoBehaviour
     {
-   
         public void LoadScene(string sceneName)
         {
             LevelManager.LoadScene(sceneName);
