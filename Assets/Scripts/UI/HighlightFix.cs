f0ec2afef805fefdca2bfa6fc1c4f94ac12a9850
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-07 23:56:22 +0300

    Onpointer and ondeselect are changed

--- Assets/Scripts/UI/HighlightFix.cs
+++ Assets/Scripts/UI/HighlightFix.cs
@@ -7,15 +7,15 @@ namespace UI
     [RequireComponent(typeof(Selectable))]
     public class HighlightFix : MonoBehaviour, IPointerEnterHandler, IDeselectHandler
     {
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            if (!EventSystem.current.alreadySelecting)
-                EventSystem.current.SetSelectedGameObject(this.gameObject);
-        }
-
         public void OnDeselect(BaseEventData eventData)
         {
             GetComponent<Selectable>().OnPointerExit(null);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (!EventSystem.current.alreadySelecting)
+                EventSystem.current.SetSelectedGameObject(gameObject);
         }
     }
 }
