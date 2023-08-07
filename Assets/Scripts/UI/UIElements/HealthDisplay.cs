d7a4558de693835edc2de857696e4807cc32bcc8
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-07 23:57:15 +0300

    Update HealthDisplay.cs

--- Assets/Scripts/UI/UIElements/HealthDisplay.cs
+++ Assets/Scripts/UI/UIElements/HealthDisplay.cs
@@ -5,13 +5,14 @@ using Utility;
 
 namespace UI.UIElements
 {
-    public class HealthDisplay : global::UI.UIElements.UIElement
+    public class HealthDisplay : UIElement
     {
-        [Header("Settings")]
-        [Tooltip("The image which represents one unit of health")]
-        public GameObject healthDisplayImage = null;
+        [Header("Settings")] [Tooltip("The image which represents one unit of health")]
+        public GameObject healthDisplayImage;
+
         [Tooltip("The prefab to use to display the number")]
-        public GameObject numberDisplay = null;
+        public GameObject numberDisplay;
+
         [Tooltip("The maximum number of images to display before switching to just a number")]
         public int maximumNumberToDisplay = 3;
 
@@ -20,43 +21,34 @@ namespace UI.UIElements
         {
             if (GameManager.Instance != null && GameManager.Instance.player != null)
             {
-                Health playerHealth = GameManager.Instance.player.GetComponent<Health>();
-                if (playerHealth != null)
-                {
-                    SetChildImageNumber(playerHealth.currentHealth);
-                }
+                var playerHealth = GameManager.Instance.player.GetComponent<Health>();
+                if (playerHealth != null) SetChildImageNumber(playerHealth.currentHealth);
             }
         }
 
         /// <summary>
-        /// Description:
-        /// Deletes and spawns images until this gameobject has as many children as the player has health
-        /// Input: 
-        /// int
-        /// Return: 
-        /// void (no return)
+        ///     Description:
+        ///     Deletes and spawns images until this gameobject has as many children as the player has health
+        ///     Input:
+        ///     int
+        ///     Return:
+        ///     void (no return)
         /// </summary>
         /// <param name="number">The number of images that this object should have as children</param>
         private void SetChildImageNumber(int number)
         {
-            for (int i = transform.childCount - 1; i >= 0; i--)
-            {
-                Destroy(transform.GetChild(i).gameObject);
-            }
+            for (var i = transform.childCount - 1; i >= 0; i--) Destroy(transform.GetChild(i).gameObject);
 
             if (healthDisplayImage != null)
             {
                 if (maximumNumberToDisplay >= number)
                 {
-                    for (int i = 0; i < number; i++)
-                    {
-                        Instantiate(healthDisplayImage, transform);
-                    }
+                    for (var i = 0; i < number; i++) Instantiate(healthDisplayImage, transform);
                 }
                 else
                 {
                     Instantiate(healthDisplayImage, transform);
-                    GameObject createdNumberDisp = Instantiate(numberDisplay, transform);
+                    var createdNumberDisp = Instantiate(numberDisplay, transform);
                     createdNumberDisp.GetComponent<Text>().text = number.ToString();
                 }
             }
