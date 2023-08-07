17b7e5df53a42f314b539b09ecaa86aff6b19089
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-07 23:57:16 +0300

    Update UIPage.cs

--- Assets/Scripts/UI/UIPage.cs
+++ Assets/Scripts/UI/UIPage.cs
@@ -8,14 +8,13 @@ namespace UI
         [Tooltip("The default UI to have selected when opening this page")]
         public GameObject defaultSelected;
 
-   
+
         public void SetSelectedUIToDefault()
         {
             if (GameManager.Instance == null || GameManager.Instance.uiManager == null ||
                 defaultSelected == null) return;
             GameManager.Instance.uiManager.eventSystem.SetSelectedGameObject(null);
             GameManager.Instance.uiManager.eventSystem.SetSelectedGameObject(defaultSelected);
-
         }
     }
 }
