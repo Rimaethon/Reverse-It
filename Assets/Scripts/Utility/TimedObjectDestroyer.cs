823e2203ead34fa00f2e5b4b568f07352226aa89
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-07 23:58:23 +0300

    Update TimedObjectDestroyer.cs

--- Assets/Scripts/Utility/TimedObjectDestroyer.cs
+++ Assets/Scripts/Utility/TimedObjectDestroyer.cs
@@ -4,53 +4,46 @@ namespace Utility
 {
     public class TimedObjectDestroyer : MonoBehaviour
     {
-        [Header("Settings:")]
-        [Tooltip("The lifetime of this gameobject")]
-        public float lifetime = 5.0f;
+        private static bool _quitting;
 
-        private float _timeAlive;
+        [Header("Settings:")] [Tooltip("The lifetime of this gameobject")]
+        public float lifetime = 5.0f;
 
         [Tooltip("Whether or not to destroy child gameobjects when this gameobject is destroyed")]
         public bool destroyChildrenOnDeath = true;
+
+        private float _timeAlive;
 
 
         private void Update()
         {
             if (_timeAlive > lifetime)
-            {
-                Destroy(this.gameObject);
-            }
+                Destroy(gameObject);
             else
-            {
                 _timeAlive += Time.deltaTime;
-            }
         }
 
-        private static bool _quitting;
 
-       
-        private void OnApplicationQuit()
-        {
-            _quitting = true;
-            DestroyImmediate(this.gameObject);
-        }
-
-      
         private void OnDestroy()
         {
             if (destroyChildrenOnDeath && !_quitting && Application.isPlaying)
             {
-                int childCount = transform.childCount;
-                for (int i = childCount - 1; i >= 0; i--)
+                var childCount = transform.childCount;
+                for (var i = childCount - 1; i >= 0; i--)
                 {
-                    GameObject childObject = transform.GetChild(i).gameObject;
-                    if (childObject != null)
-                    {
-                        Destroy(childObject);
-                    }
+                    var childObject = transform.GetChild(i).gameObject;
+                    if (childObject != null) Destroy(childObject);
                 }
             }
+
             transform.DetachChildren();
+        }
+
+
+        private void OnApplicationQuit()
+        {
+            _quitting = true;
+            DestroyImmediate(gameObject);
         }
     }
 }
