2d35090a6d9cc484e46316945794534766ca9768
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-07 23:56:47 +0300

    Update ScoreDisplay.cs

--- Assets/Scripts/UI/UIElements/ScoreDisplay.cs
+++ Assets/Scripts/UI/UIElements/ScoreDisplay.cs
@@ -1,49 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UI.UIElements;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 using Utility;
 
-/// <summary>
-/// This class handles displayig the score
-/// </summary>
-public class ScoreDisplay : UIElement
+namespace UI.UIElements
 {
-    [Header("References")]
-    [Tooltip("The text to use when displaying the score")]
-    public Text displayText = null;
-
-    /// <summary>
-    /// Description:
-    /// Displays the score onto the display text
-    /// Input:
-    /// none
-    /// Return:
-    /// void (no return)
-    /// </summary>
-    public void DisplayScore()
+    public class ScoreDisplay : UIElement
     {
-        if (displayText != null)
+        public Text displayText;
+
+
+        private void DisplayScore()
         {
-            displayText.text = "Score: " + GameManager.Score.ToString();
+            if (displayText != null) displayText.text = "Score: " + GameManager.Score;
         }
-    }
 
-    /// <summary>
-    /// Description:
-    /// Updates this UI based on this class
-    /// Input:
-    /// none
-    /// Return:
-    /// void (no return)
-    /// </summary>
-    public override void UpdateUI()
-    {
-        // This calls the base update UI function from the UIelement class
-        base.UpdateUI();
 
-        // The remaining code is only called for this sub-class of UIelement and not others
-        DisplayScore();
+        public override void UpdateUI()
+        {
+            base.UpdateUI();
+
+            DisplayScore();
+        }
     }
 }
