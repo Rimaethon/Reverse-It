6062883cd8851b4620e041c49e53b602ec24a25b
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-10 23:57:33 +0300

    Checkpoint script refactored

--- Assets/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -1,31 +1,38 @@
-﻿using Health_Damage;
+﻿using System;
+using Health_Damage;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 namespace Checkpoint
 {
     public class Checkpoint : MonoBehaviour
     {
-        [SerializeField] private Transform respawnLocation;
-        [SerializeField] private Animator checkpointAnimator;
+        private Vector3 m_RespawnLocation;
+        private Animator m_CheckpointAnimator;
         [SerializeField] private string animatorActiveParameter = "isActive";
         [SerializeField] private GameObject checkpointActivationEffect;
 
+        private void Awake()
+        {
+            m_RespawnLocation = gameObject.transform.position;
+        }
+        
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (!collision.CompareTag("Player") || collision.gameObject.GetComponent<Health>() == null) return;
 
             var playerHealth = collision.gameObject.GetComponent<Health>();
-            playerHealth.SetRespawnPoint(respawnLocation.position);
+            playerHealth.SetRespawnPoint(m_RespawnLocation);
 
             if (CheckpointTracker.CurrentCheckpoint != null)
-                CheckpointTracker.CurrentCheckpoint.checkpointAnimator.SetBool(animatorActiveParameter, false);
+                CheckpointTracker.CurrentCheckpoint.m_CheckpointAnimator.SetBool(animatorActiveParameter, false);
 
             if (CheckpointTracker.CurrentCheckpoint != this && checkpointActivationEffect != null)
                 Instantiate(checkpointActivationEffect, transform.position, Quaternion.identity, null);
 
             CheckpointTracker.CurrentCheckpoint = this;
-            checkpointAnimator.SetBool(animatorActiveParameter, true);
+            m_CheckpointAnimator.SetBool(animatorActiveParameter, true);
         }
     }
 }
