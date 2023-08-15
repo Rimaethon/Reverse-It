7b45664cffdc5dab6db5779b989b6112b25a5066
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-16 01:42:53 +0300

    Add BaseHealth class for damage handling
    This commit introduces a new BaseHealth class under the Health&Damaged namespace. It's an abstract class representing a customizable starting point for any damageable in-game entity. The class includes functionality for handling health reduction, team identification, and invincibility mechanics. This is part of an overhaul to unify and streamline how damage is managed across different game elements.

---
+++ Assets/Rimaethon/Scripts/Health&Damage/BaseHealth.cs
@@ -0,0 +1,38 @@
+﻿using Rimaethon.Scripts.Core.Interfaces;
+using UnityEngine;
+
+namespace Rimaethon.Scripts.Health_Damage
+{
+    public abstract class BaseHealth : MonoBehaviour, IDamageable
+    {
+        protected int m_TeamId;
+        public int TeamId { get; }
+
+        protected bool m_IsInvincible;
+        private float m_InvincibilityTime = 3f;
+        protected float _timeToBecomeDamagableAgain;
+
+        public int Health
+        {
+            get => m_Health;
+            protected set => m_Health = Mathf.Clamp(value, 0, 100);
+        }
+        private int m_Health;
+
+        public void TakeDamage(IDamageSource source)
+        {
+            if (m_IsInvincible) return;
+            Health -= source.DamageAmount;
+        }
+
+        protected virtual void Update()
+        {
+            InvincibilityCheck();
+        }
+
+        private void InvincibilityCheck()
+        {
+            if (_timeToBecomeDamagableAgain <= Time.time) m_IsInvincible = false;
+        }
+    }
+}
