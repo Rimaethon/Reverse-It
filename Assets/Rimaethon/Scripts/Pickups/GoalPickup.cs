6f9a9cce0b3159338d0da55ad0e4083cd51bc1fc
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-18 23:48:35 +0300

    Refactor GoalPickup script to accommodate new namespaces
    The GoalPickup script has been refactored to use new namespaces for the Health_Damage and Utility components. This change was necessary due to a re-organization of the project's structure to better categorize scripts. Additionally, the health component check within the DoOnPickup function was updated to the newly renamed BaseHealth. This ensures compatibility with the revised health system.

--- Assets/Rimaethon/Scripts/Pickups/GoalPickup.cs
+++ Assets/Rimaethon/Scripts/Pickups/GoalPickup.cs
@@ -1,14 +1,14 @@
-﻿using Health_Damage;
+﻿using Rimaethon.Scripts.Health_Damage;
+using Rimaethon.Scripts.Utility;
 using UnityEngine;
-using Utility;
 
-namespace Pickups
+namespace Rimaethon.Scripts.Pickups
 {
     public class GoalPickup : Pickup
     {
         protected override void DoOnPickup(Collider2D collision)
         {
-            if (collision.CompareTag("Player") && collision.gameObject.GetComponent<Health>() != null)
+            if (collision.CompareTag("Player") && collision.gameObject.GetComponent<BaseHealth>() != null)
                 if (GameManager.Instance != null)
                     GameManager.Instance.LevelCleared();
             base.DoOnPickup(collision);
