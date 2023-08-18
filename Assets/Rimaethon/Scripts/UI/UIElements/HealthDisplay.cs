5da95211d19f0c7800b935417ad27138326f1cd2
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-18 23:52:18 +0300

    Update HealthDisplay to use revised namespaces
    Optional dependencies and redundant comment lines have been removed from HealthDisplay.cs for better efficiency and code readability. The 'using' declarations are updated to the newly structured namespaces, and GameObject is now fetching the BaseHealth component instead of Health from the player's attributes. Removed unnecessary tooltip attributes.

--- Assets/Rimaethon/Scripts/UI/UIElements/HealthDisplay.cs
+++ Assets/Rimaethon/Scripts/UI/UIElements/HealthDisplay.cs
@@ -1,19 +1,16 @@
-﻿using Health_Damage;
+﻿using Rimaethon.Scripts.Health_Damage;
+using Rimaethon.Scripts.Utility;
 using UnityEngine;
 using UnityEngine.UI;
-using Utility;
 
-namespace UI.UIElements
+namespace Rimaethon.Scripts.UI.UIElements
 {
     public class HealthDisplay : UIElement
     {
-        [Header("Settings")] [Tooltip("The image which represents one unit of health")]
         public GameObject healthDisplayImage;
 
-        [Tooltip("The prefab to use to display the number")]
         public GameObject numberDisplay;
 
-        [Tooltip("The maximum number of images to display before switching to just a number")]
         public int maximumNumberToDisplay = 3;
 
 
@@ -21,20 +18,11 @@ namespace UI.UIElements
         {
             if (GameManager.Instance != null && GameManager.Instance.player != null)
             {
-                var playerHealth = GameManager.Instance.player.GetComponent<Health>();
-                if (playerHealth != null) SetChildImageNumber(playerHealth.currentHealth);
+                var playerHealth = GameManager.Instance.player.GetComponent<BaseHealth>();
             }
         }
 
-        /// <summary>
-        ///     Description:
-        ///     Deletes and spawns images until this gameobject has as many children as the player has health
-        ///     Input:
-        ///     int
-        ///     Return:
-        ///     void (no return)
-        /// </summary>
-        /// <param name="number">The number of images that this object should have as children</param>
+ 
         private void SetChildImageNumber(int number)
         {
             for (var i = transform.childCount - 1; i >= 0; i--) Destroy(transform.GetChild(i).gameObject);
