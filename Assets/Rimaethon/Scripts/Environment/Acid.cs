1836f70d79e00aa757c5d8b1199f9d1295608b64
Yoldaş Ulaş <yutalas@gmail.com>
2023-08-18 23:38:09 +0300

    Refactor Acid class to inherit from DamageBase
    Acid class was previously implementing IDamageSource, but has been refracted to inherit from DamageBase instead. This change simplifies the code and allows for better reuse of common damage-related functionalities. Moreover, OnTriggerStay method was added to apply damage where necessary. This change makes the interaction more intuitive and gameplay smoother.

--- Assets/Rimaethon/Scripts/Environment/Acid.cs
+++ Assets/Rimaethon/Scripts/Environment/Acid.cs
@@ -1,18 +1,23 @@
+using System;
 using Rimaethon.Scripts.Core.Interfaces;
+using Rimaethon.Scripts.Health_Damage;
 using UnityEngine;
 
 namespace Rimaethon.Scripts.Environment
 {
-    public class Acid : MonoBehaviour,IDamageSource
+    public class Acid : DamageBase
     {
         public int DamageAmount { get; }
-
         public void ApplyDamage(IDamageable target)
         {
             target.TakeDamage(this);
 
         }
-        
-        
+
+        private void OnTriggerStay(Collider other)
+        {
+            other.gameObject.TryGetComponent(out IDamageable damageable);
+            ApplyDamage(damageable);
+        }
     }
 }
