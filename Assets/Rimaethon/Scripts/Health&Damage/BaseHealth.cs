61c6b51b03f7ccb6c7d4d034f5500191c0c41994
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-09 23:50:38 +0300

    Refactor BaseHealth script and add health change event
    BaseHealth script was refactored for improved readability and maintainability. The protection level of m_IsInvincible variable was changed from protected to private. A broadcast event was added in TakeDamage method to communicate health changes to other parts of the game and allow updates in real-time.

--- Assets/Rimaethon/Scripts/Health&Damage/BaseHealth.cs
+++ Assets/Rimaethon/Scripts/Health&Damage/BaseHealth.cs
@@ -1,4 +1,6 @@
-﻿using Rimaethon.Scripts.Core.Interfaces;
+﻿using Rimaethon.Scripts.Core.Enums;
+using Rimaethon.Scripts.Core.Interfaces;
+using Rimaethon.Scripts.Managers;
 using UnityEngine;
 
 namespace Rimaethon.Scripts.Health_Damage
@@ -7,22 +9,22 @@ namespace Rimaethon.Scripts.Health_Damage
     {
         protected int m_TeamId;
         public int TeamId { get; }
-
-        protected bool m_IsInvincible;
+        private bool m_IsInvincible;
         private float m_InvincibilityTime = 3f;
         protected float _timeToBecomeDamagableAgain;
+        private int m_Health=100;
 
         public int Health
         {
             get => m_Health;
             protected set => m_Health = Mathf.Clamp(value, 0, 100);
         }
-        private int m_Health=100;
 
         public void TakeDamage(IDamageSource source)
         {
             if (m_IsInvincible) return;
             Health -= source.DamageAmount;
+            EventManager.Instance.Broadcast(GameEvents.OnHealthChange, Health);
         }
 
         protected virtual void Update()
