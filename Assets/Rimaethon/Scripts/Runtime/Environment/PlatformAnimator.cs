51c06ce615433df30025de6af7c740193e07385e
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-25 11:23:55 +0300

    Platform Animator is Updated
    It was a bad idea to check for WaypointMover.cs and Animator each time while moving the platform. So I added them as required components and moved the logic into fixed update

---
+++ Assets/Rimaethon/Scripts/Runtime/Environment/PlatformAnimator.cs
@@ -0,0 +1,26 @@
+﻿using Rimaethon.Scripts.Environment;
+using UnityEngine;
+
+namespace Rimaethon.Runtime.Environment
+{
+    [RequireComponent(typeof(WaypointMover))]
+    [RequireComponent(typeof(Animator))]
+    public class PlatformAnimator : MonoBehaviour
+    {
+        private static readonly int IsMoving = Animator.StringToHash("isMoving");
+        private Animator _animator;
+        private WaypointMover _mover;
+
+        private void Awake()
+        {
+            _mover = GetComponent<WaypointMover>();
+            _animator = GetComponent<Animator>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            _animator.SetBool(IsMoving, !_mover.stopped);
+        }
+    }
+}
