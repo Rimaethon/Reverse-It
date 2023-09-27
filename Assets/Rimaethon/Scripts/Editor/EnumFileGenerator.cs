16e166aad1b6180508defb0a75ed1e146347adb3
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-27 17:21:52 +0300

    Enum File Generator Editor Script is created
    This is my first editor script and I believe it does its job pretty well. It creates enums based on the names of the Audio files inside Audio Library scriptable object and saves them. I'm not sure but I believe this is also what people calls code generation.

---
+++ Assets/Rimaethon/Scripts/Editor/EnumFileGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using Rimaethon.Runtime.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rimaethon.Scripts.Editor
+{
+    [CustomEditor(typeof(AudioLibrary))]
+    public class EnumFileGenerator : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var sfxLibrary = (AudioLibrary)target;
+
+            if (GUILayout.Button("Generate Enums")) GenerateEnums(sfxLibrary);
+        }
+
+        private void GenerateEnums(AudioLibrary sfxLibrary)
+        {
+            foreach (var fieldInfo in sfxLibrary.GetType().GetFields())
+                if (fieldInfo.FieldType == typeof(AudioClip[]))
+                {
+                    var arrayName = fieldInfo.Name;
+                    var enumCode = $"public enum {arrayName}\n{{\n";
+
+                    var audioClips = (AudioClip[])fieldInfo.GetValue(sfxLibrary);
+
+                    for (var i = 0; i < audioClips.Length; i++) enumCode += $"\t{audioClips[i].name} = {i},\n";
+
+                    enumCode += "}";
+
+                    Debug.Log($"Generated Enum for {arrayName}:\n{enumCode}");
+
+                    SaveEnumToFile(arrayName, enumCode);
+                }
+        }
+
+        private void SaveEnumToFile(string arrayName, string enumCode)
+        {
+            var directoryPath = "Assets/Rimaethon/Scripts/Runtime/Core/Enums";
+            var filePath = $"{directoryPath}/{arrayName}.cs";
+
+            if (!AssetDatabase.IsValidFolder(directoryPath))
+                AssetDatabase.CreateFolder("Assets/Rimaethon/Scripts/Runtime/Core", "Enums");
+
+            File.WriteAllText(filePath, enumCode);
+            AssetDatabase.Refresh();
+            Debug.Log($"Saved Enum for {arrayName} to {filePath}");
+        }
+    }
+}
