7e883f85b2a3684897e85463d6f57397e8d3c29a
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-27 10:28:01 +0300

    Door script's animation event invoking replaced with reference based invocation
    Previous version of Door.cs was handling animations with firing an event which was not really understandable and it was unnecessary to use events instead of calling animations by reference.

---
+++ Assets/Rimaethon/Scripts/Runtime/Keys&Doors/Door.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace Rimaethon.Runtime.Keys_Doors
+{
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class Door : MonoBehaviour
+    {
+        [SerializeField] private int doorID;
+        private readonly int _closeHash = Animator.StringToHash("close");
+        private readonly int _openHash = Animator.StringToHash("open");
+        private Animator _animator;
+        private BoxCollider2D _boxCollider;
+        private bool _isOpen;
+
+
+        private void Awake()
+        {
+            _boxCollider = GetComponent<BoxCollider2D>();
+            _animator = GetComponent<Animator>();
+        }
+
+
+        private void OnDisable()
+        {
+            KeyRing.ClearKeyRing();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player")) AttemptToOpen();
+        }
+
+        private void AttemptToOpen()
+        {
+            if (!CheckPlayerHasKey() || _isOpen) return;
+            Open();
+            //AudioManager.Instance.PlaySFX(3);
+        }
+
+
+        private bool CheckPlayerHasKey()
+        {
+            return KeyRing.HasKey(doorID);
+        }
+
+
+        private void Open()
+        {
+            _isOpen = true;
+            _animator.SetTrigger(_openHash);
+            //AudioManager.Instance.PlaySFX(2);
+        }
+
+        public void DisableCollider()
+        {
+            _boxCollider.enabled = false;
+        }
+
+        private void Close()
+        {
+            _isOpen = false;
+            _animator.SetTrigger(_closeHash);
+            _boxCollider.enabled = true;
+            // AudioManager.Instance.PlaySFX(2);
+        }
+    }
+}
