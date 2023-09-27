556d51783f75ce41e6b88016aaa0cb19e4bae6d0
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-27 12:14:11 +0300

    Goal Pickup And Pickup Base Classes are updated
    Instead of checking for tags in DoOnPickup I find it better to make the check before calling the DoOnPickup method.

---
+++ Assets/Rimaethon/Scripts/Runtime/Pickups/GoalPickup.cs
@@ -0,0 +1,21 @@
+﻿using Rimaethon.Scripts.Core.Enums;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Rimaethon.Runtime.Pickups
+{
+    public class GoalPickup : Pickup
+    {
+        [SerializeField] private int sceneIndex;
+
+        protected override void DoOnPickup(Collider2D collision)
+        {
+            AudioManager.Instance.PlaySFX(SFXClips.GoalPickup);
+
+            EventManager.Instance.Broadcast(GameEvents.OnSceneChange, sceneIndex);
+
+
+            base.DoOnPickup(collision);
+        }
+    }
+}
--- Assets/Rimaethon/Scripts/Runtime/Pickups/Pickup.cs
+++ Assets/Rimaethon/Scripts/Runtime/Pickups/Pickup.cs
@@ -1,21 +1,19 @@
 ﻿using UnityEngine;
 
-namespace Rimaethon.Scripts.Pickups
+namespace Rimaethon.Runtime.Pickups
 {
     public class Pickup : MonoBehaviour
     {
-        [SerializeField] private GameObject pickUpEffect;
 
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            if (!collision.CompareTag("Player")) return;
             DoOnPickup(collision);
         }
 
         protected virtual void DoOnPickup(Collider2D collision)
         {
-            if (!collision.CompareTag("Player")) return;
-            if (pickUpEffect != null) Instantiate(pickUpEffect, transform.position, Quaternion.identity, null);
             Destroy(gameObject);
         }
     }
