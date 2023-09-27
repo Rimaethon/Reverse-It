58960f792959f7315c8ca4ed7f8d0c363f70f4c5
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-27 14:41:06 +0300

    Audio Library scriptable object is created
    I'm trying to create a generic Audio container that will be easily scalable. Its on thought progress right now.

---
+++ Assets/Rimaethon/Scripts/Runtime/Scriptable Objects/AudioLibrary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Rimaethon.Runtime.Core
+{
+    [CreateAssetMenu(fileName = "AudioLibrary", menuName = "Rimaethon/Audio Library", order = 1)]
+    public class AudioLibrary : ScriptableObject
+    {
+        public AudioClip[] SFXClips;
+        public AudioClip[] MusicClips;
+    }
+}
