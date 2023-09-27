8c6f7fd7404e94f1f0150182b4e8c1bf7b666995
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-27 12:47:40 +0300

    Gem Pickup And Red Gem Pickup Classes are created
    I thought it would be a good idea to make the pickup system more Object Oriented instead of writing same things over and over again.

---
+++ Assets/Rimaethon/Scripts/Runtime/Pickups/GemPickup.cs
@@ -0,0 +1,20 @@
+﻿using Rimaethon.Runtime.Pickups;
+using Rimaethon.Scripts.Core.Enums;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Rimaethon.Scripts.Pickups
+{
+    public class GemPickup : Pickup
+    {
+        [SerializeField] private int scoreAmount = 1;
+
+
+        protected override void DoOnPickup(Collider2D collision)
+        {
+            base.DoOnPickup(collision);
+            AudioManager.Instance.PlaySFX(SFXClips.ScorePickUp);
+            EventManager.Instance.Broadcast(GameEvents.OnGemCollected, scoreAmount);
+        }
+    }
+}
---
+++ Assets/Rimaethon/Scripts/Runtime/Pickups/RedGemPickup.cs
@@ -0,0 +1,19 @@
+﻿using Rimaethon.Runtime.Pickups;
+using Rimaethon.Scripts.Core.Enums;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Rimaethon.Scripts.Pickups
+{
+    public class RedGemPickup : GemPickup
+    {
+
+
+
+        protected override void DoOnPickup(Collider2D collision)
+        {
+            base.DoOnPickup(collision);
+            EventManager.Instance.Broadcast(GameEvents.OnRedGemCollected);
+        }
+    }
+}
