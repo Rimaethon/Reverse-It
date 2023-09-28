db3ebc571ab17c9b27af7e3d8f620fe606f123f9
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-28 13:57:37 +0300

    Save and UI related methods are removed from Game Manager class
    It was confusing since it was handling too many things.

---
+++ Assets/Rimaethon/Scripts/Runtime/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using Rimaethon.Scripts.Utility;
+using UnityEngine;
+
+
+namespace Rimaethon.Runtime.Managers
+{
+    
+    public class GameManager : PersistentSingleton<GameManager>
+    {
+    
+        private int count1;
+        private int count2;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Application.targetFrameRate = -1;
+        }
+        
+        
+        public void ResetPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+     
+    }
+}
