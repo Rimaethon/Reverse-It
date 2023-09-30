4663032fe0ecc6aa3d3d034a8586490b7f5d4135
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-30 11:29:11 +0300

    Base Health, Damage Base , Enemy Health, Player Health, Player Damager scripts are replaced with Character Combat
    There was too many classes for a simple health and damage system. So I replaced them with a generic CharacterCombat class

---
+++ Assets/Rimaethon/Scripts/Runtime/Core/CharacterCombat.cs
@@ -0,0 +1,74 @@
+﻿using Rimaethon.Runtime.Scriptable_Objects;
+using Rimaethon.Scripts.Core.Interfaces;
+using UnityEngine;
+
+namespace Rimaethon.Runtime.Core
+{
+    public class CharacterCombat : MonoBehaviour, IDamageAble
+    {
+        [SerializeField] protected CharacterConfigSO characterConfigSo;
+        private float _invincibilityDuration;
+
+        private float _lastDamageTime;
+        protected int _teamId;
+        protected float ContactNormal;
+        protected int DamageAmount;
+
+        protected int Health;
+
+        protected virtual void Awake()
+        {
+            _teamId = characterConfigSo.teamId;
+            DamageAmount = characterConfigSo.damageAmount;
+            Health = characterConfigSo.maxHealth;
+            _invincibilityDuration = characterConfigSo.invincibilityDuration;
+        }
+
+        public int TeamId => _teamId;
+
+
+        public void TakeDamage(int damageAmount, float contactNormal)
+        {
+            if (IsInvincible() || Health <= 0) return;
+
+            if (Health - damageAmount <= 0)
+            {
+                Health = 0;
+                HandleDeath();
+            }
+            else
+            {
+                Health -= damageAmount;
+                HandleDamage(contactNormal);
+            }
+        }
+
+        protected virtual void HandleDeath()
+        {
+        }
+
+        protected virtual void HandleDamage()
+        {
+        }
+
+        protected virtual void HandleDamage(float contactNormal)
+        {
+        }
+
+        protected virtual bool IsInvincible()
+        {
+            var currentTime = Time.time;
+
+            if (_lastDamageTime == 0)
+            {
+                _lastDamageTime = currentTime;
+                return false;
+            }
+
+            if (currentTime - _lastDamageTime < _invincibilityDuration) return true;
+
+            _lastDamageTime = currentTime;
+            return false;
+        }
+    }
+}
