3e9e3bf61d7a607718b027697ec91d25a06cbb18
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-30 16:15:48 +0300

    Player Health Config Scriptable Object and Player Respawn Handler class is created
    Player health config will held the player's combat related data and player respawn handler will spawn the player to nearest checkpoint when he/she is dead

---
+++ Assets/Rimaethon/Scripts/Runtime/Player/PlayerRespawnHandler.cs
@@ -0,0 +1,35 @@
+﻿using Rimaethon.Runtime.Checkpoint;
+using Rimaethon.Scripts.Core.Enums;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Rimaethon.Runtime.Player
+{
+    public class PlayerRespawnHandler : MonoBehaviour
+    {
+        private Vector3 _spawnPosition;
+
+        private void Awake()
+        {
+            _spawnPosition = transform.position;
+        }
+
+        private void OnEnable()
+        {
+            EventManager.Instance.AddHandler(GameEvents.OnPlayerRespawned, OnPlayerRespawn);
+        }
+
+        private void OnDisable()
+        {
+            if (EventManager.Instance == null) return;
+            EventManager.Instance.RemoveHandler(GameEvents.OnPlayerRespawned, OnPlayerRespawn);
+        }
+
+        private void OnPlayerRespawn()
+        {
+            transform.position = CheckpointTracker.CurrentCheckpoint == null
+                ? _spawnPosition
+                : CheckpointTracker.CurrentCheckpoint.transform.position;
+        }
+    }
+}
