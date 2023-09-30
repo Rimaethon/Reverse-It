1a4c486c6bb8a3a8c8894f363b556ab2581dc12b
Yoldaş Ulaş <yutalas@gmail.com>
2023-09-30 14:00:40 +0300

    PlayerCombat.cs is created
    This class will handle revive and hitting to the enemy logic

---
+++ Assets/Rimaethon/Scripts/Runtime/Player/PlayerCombat.cs
@@ -0,0 +1,59 @@
+using Rimaethon.Runtime.Core;
+using Rimaethon.Scripts.Core.Enums;
+using Rimaethon.Scripts.Core.Interfaces;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Rimaethon.Runtime.Player
+{
+    public class PlayerCombat : CharacterCombat
+    {
+        private void Start()
+        {
+            EventManager.Instance.Broadcast(GameEvents.OnHealthChange, Health);
+        }
+
+        private void OnEnable()
+        {
+            EventManager.Instance.AddHandler(GameEvents.OnPlayerRespawned, HandleRevive);
+        }
+
+        private void OnDisable()
+        {
+            if (EventManager.Instance == null) return;
+
+            EventManager.Instance.RemoveHandler(GameEvents.OnPlayerRespawned, HandleRevive);
+        }
+
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("EnemyHead"))
+                if (other.gameObject.TryGetComponent(out IDamageAble damageable))
+                {
+                    if (damageable.TeamId == _teamId) return;
+
+                    damageable.TakeDamage(DamageAmount);
+                }
+        }
+
+
+        private void HandleRevive()
+        {
+            Health = characterConfigSo.maxHealth;
+            EventManager.Instance.Broadcast(GameEvents.OnHealthChange, Health);
+        }
+
+        protected override void HandleDeath()
+        {
+            EventManager.Instance.Broadcast(GameEvents.OnPlayerDead);
+            EventManager.Instance.Broadcast(GameEvents.OnHealthChange, Health);
+        }
+
+        protected override void HandleDamage(float contactNormal)
+        {
+            EventManager.Instance.Broadcast(GameEvents.OnPlayerDamaged, contactNormal);
+            EventManager.Instance.Broadcast(GameEvents.OnHealthChange, Health);
+        }
+    }
+}
