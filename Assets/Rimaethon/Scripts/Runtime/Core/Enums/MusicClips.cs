d357dd044e499fd5abf1f01a9a02d7160f586074
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-02 09:24:36 +0300

    First versions of MusicClips.cs And SFXClips.cs are created
    Or should I say generated. Nothing is more satisfying than clicking to a button to generate enums.(Okay there was some errors that made it harder but we don't talk about it)

---
+++ Assets/Rimaethon/Scripts/Runtime/Core/Enums/MusicClips.cs
@@ -0,0 +1,8 @@
+public enum MusicClips
+{
+    InGameMusic1 = 0,
+    InGameMusic2 = 1,
+    InGameMusic3 = 2,
+    LevelMusic = 3,
+    MenuMusic = 4
+}
---
+++ Assets/Rimaethon/Scripts/Runtime/Core/Enums/SFXClips.cs
@@ -0,0 +1,20 @@
+public enum SFXClips
+{
+    HealthPickup = 0,
+    KeyPickup = 1,
+    LifePickup = 2,
+    ScorePickUp = 3,
+    PlayerDeath = 4,
+    PlayerHurt = 5,
+    PlayerJump = 6,
+    PlayerLanding = 7,
+    EnemyDeath = 8,
+    EnemyHurt = 9,
+    DoorOpenClose = 10,
+    LockedDoor = 11,
+    CheckpointSFX = 12,
+    UIBack = 13,
+    UIForward = 14,
+    UISwitch = 15,
+    GoalPickup = 16
+}
