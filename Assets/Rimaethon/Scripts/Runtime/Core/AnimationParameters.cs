a5a3923eab70ffc56242d59a200369451d163c33
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-02 11:39:14 +0300

    AnimationParameters.cs is created
    It will held the hashed animation names and return them when needed.

---
+++ Assets/Rimaethon/Scripts/Runtime/Core/AnimationParameters.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Rimaethon.Scripts.Player
+{
+    public static class AnimationParameters
+    {
+        public static readonly int IsIdle = Animator.StringToHash("isIdle");
+        public static readonly int IsRunning = Animator.StringToHash("isRunning");
+        public static readonly int IsFalling = Animator.StringToHash("isFalling");
+        public static readonly int IsJumping = Animator.StringToHash("isJumping");
+        public static readonly int IsDead = Animator.StringToHash("isDead");
+        public static readonly int IsDamaged = Animator.StringToHash("isDamaged");
+    }
+}
