d1aab0ff21d7490506540c7ed690756e35fc4517
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-04 12:02:58 +0300

    HealthPickup.cs and KeyPickup.cs are updated
    Namings were bad since I'm done with using m_ also healing amount is now just returning a value instead of an additional private int.Also key pickup logic's header and tooltip is removed since it is unnecessary

---
+++ Assets/Rimaethon/Scripts/Runtime/Pickups/HealthPickup.cs
@@ -0,0 +1,26 @@
+﻿using Rimaethon.Runtime.Pickups;
+using Rimaethon.Scripts.Core.Interfaces;
+using UnityEngine;
+
+namespace Rimaethon.Scripts.Pickups
+{
+    public class HealthPickup : Pickup, IGiveHeal
+    {
+        public int HealAmount => 1;
+
+
+        public void GiveHeal(IHealAble target)
+        {
+            target.ReceiveHealing(this);
+        }
+
+
+        protected override void DoOnPickup(Collider2D collision)
+        {
+            collision.gameObject.TryGetComponent(out IHealAble health);
+            if (health == null) return;
+
+            GiveHeal(health);
+        }
+    }
+}
---
+++ Assets/Rimaethon/Scripts/Runtime/Pickups/KeyPickup.cs
@@ -0,0 +1,17 @@
+﻿using Rimaethon.Runtime.Keys_Doors;
+using UnityEngine;
+
+namespace Rimaethon.Runtime.Pickups
+{
+    public class KeyPickup : Pickup
+    {
+        [SerializeField] private int keyID;
+
+
+        protected override void DoOnPickup(Collider2D collision)
+        {
+            base.DoOnPickup(collision);
+            KeyRing.AddKey(keyID);
+        }
+    }
+}
