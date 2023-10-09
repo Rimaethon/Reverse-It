834247ba66a934394fd24477ef528e86484cc778
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-09 09:16:14 +0300

    QuitGameButton.cs and SceneChangerButton.cs are created
    As their name explains they are handling scene changing and quitting from the game.

---
+++ Assets/Rimaethon/Scripts/Runtime/UI/Buttons/QuitGameButton.cs
@@ -0,0 +1,19 @@
+﻿using Rimaethon.Runtime.UI;
+
+namespace Rimaethon.Scripts.UI
+{
+    public class QuitGameButton : UIButton
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+
+            Button.onClick.AddListener(QuitGame);
+        }
+
+        private void QuitGame()
+        {
+
+        }
+    }
+}
---
+++ Assets/Rimaethon/Scripts/Runtime/UI/Buttons/SceneChangerButton.cs
@@ -0,0 +1,24 @@
+﻿using Rimaethon.Runtime.Managers;
+using Rimaethon.Scripts.Core.Enums;
+using Rimaethon.Scripts.Managers;
+using UnityEngine;
+
+namespace Rimaethon.Runtime.UI
+{
+    public class SceneChangerButton : UIButton
+    {
+        [SerializeField] private int sceneIndex;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Button.onClick.AddListener(ChangeScene);
+        }
+
+        private void ChangeScene()
+        {
+            
+            EventManager.Instance.Broadcast(GameEvents.OnSceneChange, sceneIndex);
+        }
+    }
+}
