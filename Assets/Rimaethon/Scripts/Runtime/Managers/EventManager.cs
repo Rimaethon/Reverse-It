fd01f997f062557704cf13485e4624cad3a2238c
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-11 10:50:29 +0300

    Capability to show Event Handlers for each event in the inspector added to EventManager.cs
    Now at start it takes all the added handlers and stores them in a list as strings.

--- Assets/Rimaethon/Scripts/Runtime/Managers/EventManager.cs
+++ Assets/Rimaethon/Scripts/Runtime/Managers/EventManager.cs
@@ -7,24 +7,50 @@ using UnityEngine;
 
 namespace Rimaethon.Scripts.Managers
 {
-    public class EventManager : Singleton<EventManager>
+    public class EventManager : PersistentSingleton<EventManager>
     {
         #region Fields And Properties
-
-        private readonly Dictionary<GameEvents, List<Delegate>> _eventHandlers = new Dictionary<GameEvents, List<Delegate>>();
-        
+        private readonly Dictionary<GameEvents, List<Delegate>> _eventHandlers = new();
+        [SerializeField] private bool showEventNames; 
         #endregion
 
-   
-     
+
+        #region Unity Methods
+
+        [SerializeField] private List<string> eventNames = new();
+
+        private void Start()
+        {
+            if (!showEventNames) return;
+            foreach (var events in _eventHandlers.Keys)
+            {
+                eventNames.Add(events.ToString());
+                foreach (var value in _eventHandlers[events].ToList())
+                {
+                    Type type = value.Method.DeclaringType;
+                    if (type == null) continue;
+                    string className = type.Name;
+                    eventNames.Add(className+ "." + value.Method.Name);
+                }
+               
+            }
+
+        }
+
+        protected override void OnApplicationQuit()
+        {
+            _eventHandlers.Clear();
+            Debug.LogWarning("Event Manager is cleared");
+            base.OnApplicationQuit();
+        }
+
+        #endregion
+
         #region Event Handlers
 
         public void AddHandler(GameEvents gameEvent, Action handler)
         {
-            if (!_eventHandlers.ContainsKey(gameEvent))
-            {
-                _eventHandlers[gameEvent] = new List<Delegate>();
-            }
+            if (!_eventHandlers.ContainsKey(gameEvent)) _eventHandlers[gameEvent] = new List<Delegate>();
 
             _eventHandlers[gameEvent].Add(handler);
             Debug.Log($"Added handler {handler.Method.Name} for game event {gameEvent}");
@@ -32,10 +58,7 @@ namespace Rimaethon.Scripts.Managers
 
         public void AddHandler<T>(GameEvents gameEvent, Action<T> handler)
         {
-            if (!_eventHandlers.ContainsKey(gameEvent))
-            {
-                _eventHandlers[gameEvent] = new List<Delegate>();
-            }
+            if (!_eventHandlers.ContainsKey(gameEvent)) _eventHandlers[gameEvent] = new List<Delegate>();
 
             _eventHandlers[gameEvent].Add(handler);
             Debug.Log($"Added handler {handler.Method.Name} for game event {gameEvent}");
@@ -43,7 +66,6 @@ namespace Rimaethon.Scripts.Managers
 
         public void RemoveHandler(GameEvents gameEvent, Action handler)
         {
-            
             if (_eventHandlers.TryGetValue(gameEvent, out var handlers))
             {
                 handlers.Remove(handler);
@@ -59,7 +81,6 @@ namespace Rimaethon.Scripts.Managers
 
         public void RemoveHandler<T>(GameEvents gameEvent, Action<T> handler)
         {
-      
             if (_eventHandlers.TryGetValue(gameEvent, out var handlers))
             {
                 handlers.Remove(handler);
@@ -90,14 +111,12 @@ namespace Rimaethon.Scripts.Managers
         private void ProcessEvent(GameEvents gameEvents, params object[] args)
         {
             if (_eventHandlers.TryGetValue(gameEvents, out var eventHandler))
-            {
                 foreach (var handler in eventHandler)
                 {
                     handler.DynamicInvoke(args);
                     Debug.Log(
                         $"Broadcasted event {gameEvents} with arguments {string.Join(", ", args.Select(arg => arg.ToString()))} to handler {handler.Method.Name}");
                 }
-            }
         }
 
         #endregion
