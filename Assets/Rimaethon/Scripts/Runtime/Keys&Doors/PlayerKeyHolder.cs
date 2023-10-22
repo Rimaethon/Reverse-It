bffa6797c0e963b97a10bc9a2009741f46b27d68
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-22 13:08:14 +0300

    KeyRing's name is changed to PlayerKeyHolder and its updated
    Now Instead of only checking if player has the key, it also checks if the ID is -1 or not so that if a door is not a locked door player can open it.

---
+++ Assets/Rimaethon/Scripts/Runtime/Keys&Doors/PlayerKeyHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rimaethon.Runtime.Keys_Doors
+{
+    public static class PlayerKeyHolder
+    {
+        private static readonly HashSet<int> keyIDs = new HashSet<int>();
+
+        public static void AddKey(int keyID)
+        {
+            keyIDs.Add(keyID);
+        }
+
+
+        public static bool HasKey(int keyID)
+        {
+            return keyIDs.Contains(keyID) || keyID == -1;
+        }
+
+
+        public static void ClearKeyRing()
+        {
+            keyIDs.Clear();
+        }
+    }
+}
