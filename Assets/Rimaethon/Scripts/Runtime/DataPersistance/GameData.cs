4c2b7bb0ad61669add554802564905d8016b3c82
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-23 15:11:39 +0300

    Icons Spritesheet.png added and GameData is created

---
+++ Assets/Rimaethon/Scripts/Runtime/DataPersistance/GameData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Rimaethon.Runtime.Checkpoint;
+using UnityEngine;
+
+[Serializable]
+public class GameData
+{
+    public long lastUpdated;
+    public int deathCount;
+    public Vector3 playerPosition;
+    public Checkpoint checkpoint;
+
+    // the values defined in this constructor will be the default values
+    // the game starts from beginning when there's no data to load
+}
