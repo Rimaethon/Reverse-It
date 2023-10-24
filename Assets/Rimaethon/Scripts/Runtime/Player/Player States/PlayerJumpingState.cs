4fe815a45c3534dc79639c5895e4b7f11516c70c
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-24 12:39:06 +0300

    PlayerJumpingState.cs is created
    Player will enter this state when user pushes the space button. It will check if its grounded or falling when executing the state.

---
+++ Assets/Rimaethon/Scripts/Runtime/Player/Player States/PlayerJumpingState.cs
@@ -0,0 +1,49 @@
+﻿using Rimaethon.Player;
+using UnityEngine;
+
+namespace Rimaethon.Scripts.Player.Player_States
+{
+    public class PlayerJumpingState : IPlayerState
+    {
+        private readonly Animator _animator;
+        private readonly PlayerController _playerController;
+        private readonly PlayerStateManager _playerStateManager;
+        private readonly Rigidbody2D _rb;
+
+        public PlayerJumpingState(PlayerController playerController, Animator animator,
+            PlayerStateManager playerStateManager, Rigidbody2D rb)
+        {
+            _playerController = playerController;
+            _animator = animator;
+            _playerStateManager = playerStateManager;
+        }
+
+        public void EnterState()
+        {
+            _animator.SetBool(AnimationParameters.IsJumping, true);
+        }
+
+        public void UpdateState()
+        {
+            CheckSwitchState();
+        }
+
+        public void ExitState()
+        {
+            _animator.SetBool(AnimationParameters.IsJumping, false);
+            _playerController.isPlayerJumping = false;
+        }
+
+        public void CheckSwitchState()
+        {
+            if (_playerController.isPlayerJumping && !_playerController.IsPlayerGrounded()) return;
+
+            if (_playerController.IsPlayerFalling()) _playerStateManager.ChangeState(_playerStateManager.AirborneState);
+
+            if (_playerController.IsPlayerGrounded())
+                _playerStateManager.ChangeState(_playerController.IsPlayerRunning()
+                    ? _playerStateManager.RunningState
+                    : _playerStateManager.GroundedState);
+        }
+    }
+}
