fe727d17131b70814a622874a30f411c9ad1701a
Yoldaş Ulaş <yutalas@gmail.com>
2023-10-30 10:24:19 +0300

    SettingsManager.cs is created
    This class will initialize the settings from GameSettingsData and save new data when needed.

---
+++ Assets/Rimaethon/Scripts/Runtime/Managers/SettingsManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Rimaethon.Runtime.DataPersistance;
+using Rimaethon.Scripts.Utility;
+using UnityEngine;
+using UnityEngine.Audio;
+using Application = UnityEngine.Device.Application;
+using Screen = UnityEngine.Device.Screen;
+
+namespace Rimaethon.Runtime.Managers
+{
+    public class SettingsManager : PersistentSingleton<GameManager>, IDataPersistence
+    {
+        [SerializeField] private AudioMixer audioMixer;
+
+        public void LoadData(GameSettingsData data)
+        {
+            Screen.fullScreen = data.FullScreen;
+            Screen.SetResolution(data.ScreenWidth, data.ScreenHeight, data.FullScreen);
+            Application.targetFrameRate = data.FrameRate;
+        }
+
+        public void SaveData(GameSettingsData data)
+        {
+        }
+    }
+}
