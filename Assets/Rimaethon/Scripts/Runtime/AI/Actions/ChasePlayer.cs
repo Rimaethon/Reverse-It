368a69844e682adb9c96e4b74fcbcc6f634ba0c3
Yoldaş Ulaş <yutalas@gmail.com>
2023-11-03 12:50:41 +0300

    Breakpoint.cs, ChasePlayer.cs and CheckPlayerAlive.cs Action node scripts are created.

---
+++ Assets/Rimaethon/Scripts/Runtime/AI/Actions/ChasePlayer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TheKiwiCoder;
+using UnityEngine;
+
+namespace Rimaethon.Scripts.AI.Runtime.Actions
+{
+    [Serializable]
+    public class ChasePlayer : ActionNode
+    {
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override State OnUpdate()
+        {
+            if (Mathf.Abs(context.PlayerData.GetPlayerPosition().x - context.Transform.position.x) < 1.5f)
+            {
+                blackboard.moveSpeed = 0;
+                context.Rigidbody2D.velocity = new Vector2(blackboard.moveSpeed, context.Rigidbody2D.velocity.y);
+                if (Mathf.Abs(context.PlayerData.GetPlayerPosition().y - context.Transform.position.y) < 0.3f)
+                    MoveToPlayer();
+            }
+            else
+            {
+                MoveToPlayer();
+                return State.Running;
+            }
+
+
+            return State.Running;
+        }
+
+        private State MoveToPlayer()
+        {
+            switch (context.PlayerData.GetPlayerPosition().x - context.Transform.position.x)
+            {
+                case > 1f:
+                    blackboard.moveSpeed = 3;
+                    context.Rigidbody2D.velocity = new Vector2(blackboard.moveSpeed, context.Rigidbody2D.velocity.y);
+                    return State.Running;
+                case < -1f:
+                    blackboard.moveSpeed = -3;
+                    context.Rigidbody2D.velocity = new Vector2(blackboard.moveSpeed, context.Rigidbody2D.velocity.y);
+                    return State.Running;
+                default:
+                    return State.Failure;
+            }
+        }
+    }
+}
---
+++ Assets/Rimaethon/Scripts/Runtime/AI/Actions/CheckPlayerAlive.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TheKiwiCoder;
+
+namespace Rimaethon.Runtime.AI.Actions
+{
+    [Serializable]
+    public class CheckPlayerAlive : DecoratorNode
+    {
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override State OnUpdate()
+        {
+            if (!context.PlayerData.IsPlayerAlive())
+                return State.Running;
+            child.Update();
+
+            return State.Running;
+        }
+    }
+}
